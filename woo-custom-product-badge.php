47d36228179e88a4321c771d73c693869465cc46
Sagnika Goswami <sagnikagoswami493@gmail.com>
2025-07-09 13:01:56 +0530

    Update woo-custom-product-badge.php

--- woo-custom-product-badge.php
+++ woo-custom-product-badge.php
@@ -2,70 +2,63 @@
 
 /**
  * Plugin Name: Woo Custom Product Badge
- * Description: This is a simple plugin to add custom badges to WooCommerce products.
+ * Description: This is simple plugin for custom product badge.
  * Author: Sagnika Goswami
  * Version: 1.0.0
  * Requires at least: 6.8.1
- * Requires PHP: 7.4
+ * Requires PHP: 8.2
  */
 
+ if(!defined('ABSPATH')){
+    exit;
+ }
 
-if (!defined('ABSPATH')) {
-  exit;
-}
+ // Register the PLugin Badge Meta Box
+ add_action('add_meta_boxes', "wcpb_register_product_badge_meta_box");
 
-// Register Product Badge Meta Box
-add_action("add_meta_boxes", "wcpb_register_product_badge_meta_box");
+ function wcpb_register_meta_box(){
+    add_meta_box("wcpb_custom_product_badge", "Woo Custom Product Badge", "wcpb_create_product_badge_meta_box", "product", "side");
+ }
 
-function wcpb_register_product_badge_meta_box()
-{
-  add_meta_box("wcpb_product_badge_meta_box", "Woo Custom Product Badge", "wpcb_create_product_badge_meta_box", "product", "side");
-}
+ // Create the Product Badge Dropdown/ Create the Meta Box for Product Badge
+ function wcpb_create_product_badge_meta_box($post){
+   $selected_badge = get_post_meta($post->ID, "wcpb_selected_badge", true);
+   ?>
+   <label for="product-badge-dropdown">Select your Product Badge</label>
+   <select name="product-badge-dropdown" id="product-badge-dropdown">
+      <option value="none" <?php if($selected_badge == "none" || !($selected_badge)){echo "selected";} ?> >None</option>
+      <option value="hot deal" <?php if($selected_badge == "hot deal"){echo "selected";} ?> >Hot Deal</option>
+      <option value="best seller" <?php if($selected_badge == "best seller"){echo "selected";} ?> >Best Seller</option>
+      <option value="new arrival" <?php if($selected_badge == "new arrival"){echo "selected";} ?> >New Arrival</option>
+   </select>
+   <?php
+ }
 
-// Creating the Meta Box Dropdown
-function wpcb_create_product_badge_meta_box($post)
-{
-  $selected_badge = get_post_meta($post->ID, "wcpb_product_badge", true);
-  if (!$selected_badge) {
-    $selected_badge = "none";
-  }
- 
-  ?>
-  <label for="badge_dropdown">Select your Product Badge</label>
-  <select name="badge_dropdown" id="badge_dropdown">
-    <option value="none" <?php if ($selected_badge == "none") {
-      echo "selected";
-    } ?>>None</option>
-    <option value="best seller" <?php if ($selected_badge == "best seller") {
-      echo "selected";
-    } ?>>Best Seller</option>
-    <option value="hot deal" <?php if ($selected_badge == "hot deal") {
-      echo "selected";
-    } ?>>Hot Deal</option>
-    <option value="new arrival" <?php if ($selected_badge == "new arrival") {
-      echo "selected";
-    } ?>>New Arrival</option>
-  </select>
-  <?php
-}
+ // Saving the Product Badge Meta Box
+ add_action("save_post", "wcpb_save_product_badge_meta_box");
 
-// Saving the Meta Box Data
-add_action("save_post", "wcpb_save_product_badge_meta_data");
+ function wcpb_save_product_badge_meta_box($post_id){
+   update_post_meta($post_id, "wcpb_selected_badge", sanitize_text_field($_POST['product-badge-dropdown']));
+ }
 
-function wcpb_save_product_badge_meta_data($post_id)
-{
-  update_post_meta($post_id, "wcpb_product_badge", $_POST['badge_dropdown']);
-}
+ // Show the Product Badge Over the Product Title
+ add_action("woocommerce_single_product_summary", "wcpb_show_product_badge");
 
-// Viewing the Product Badge 
-add_action("woocommerce_single_product_summary", "wcpb_show_product_badge");
+ function wcpb_show_product_badge(){
+   global $post, $wpdb;
 
-function wcpb_show_product_badge()
-{
-  global $post;
-  $selected_badge = get_post_meta($post->ID, "wcpb_product_badge", true);
+   $post_id = $post->ID;
+   $table = $wpdb->prefix . "postmeta";
+   $meta_key = "wcpb_selected_badge";
 
-  if ($selected_badge && $selected_badge != "none") {
+   $selected_badge = $wpdb->get_var(
+      $wpdb->prepare("SELECT meta_value FROM {$table} WHERE post_id = %d AND meta_key = %s",
+      $post_id,
+      $meta_key
+      )
+   );
+
+   if ($selected_badge && $selected_badge != "none") {
     $product_badge_final = '';
     if ($selected_badge == "best seller") {
       $product_badge_final = "Best Seller";
@@ -78,12 +71,6 @@ function wcpb_show_product_badge()
   }
 }
 
-// Adding shortcode
-  add_action("add_shortcode", "wcpb_filter_product_badge");
-
-  function wcpb_filter_product_badge(){
-    
-  }
 
 
 ?>
